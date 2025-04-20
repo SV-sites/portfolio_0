1a92bb54515d3534e15a9d5746b85faefe0a7c85
“SV-sites” <sonkauram@gmail.com>
2025-04-20 13:39:28 +0200

    base_lines_0

---
+++ msg.js
@@ -0,0 +1,14 @@
+/*
+    1 Create a function for select randomly a message.
+    2 Return the messages
+    3 Call the function
+*/
+
+console.log('Test is running!');
+
+const rand_sel_msg = () => {
+    console.log('Function is running!');
+    return 0;
+} 
+
+rand_sel_msg();
