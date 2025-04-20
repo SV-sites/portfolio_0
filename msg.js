06cd586517a2323ad824e1b3d308b16fc6633b77
“SV-sites” <sonkauram@gmail.com>
2025-04-20 13:57:57 +0200

    open_doc_func

--- msg.js
+++ msg.js
@@ -8,9 +8,15 @@ console.log('Test is running!');
 
 // task 0 create a function to open a doc file
 
+const open_doc = () => {
+    console.log('Opening document!');
+    return 0
+}
+
 const rand_sel_msg = () => {
     console.log('Function is running!');
     return 0;
 } 
 
+open_doc();
 rand_sel_msg();
